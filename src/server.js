1ffd886571b09ea3d2be47da877a5035d45e6059
ogedengbesegun <wisdomworld28608@gmail.com>
2025-09-08 22:39:55 +0100

    backMe89251039p

--- src/server.js
+++ src/server.js
@@ -256,10 +256,11 @@ await client.connect().then(() => {
     }
 
     ///look fot oraclededuct
-
+    // Get last month safely, wrap around to December if needed
+    const last2Month = monthArray[(month - 1 + 12) % 12];
     const findoraclededuct = await msc_monthly_2025.findOne({
       oracle: oraclededuct.split(',')[0],
-      month: lastMonth ?? "july", deduction: oraclededuct.split(',')[1]
+      month: lastMonth || last2Month, deduction: oraclededuct.split(',')[1]
     })
     if (!findoraclededuct) {
       return res.status(404).json({ success: false, message: "Please, Check your input and try again" })
